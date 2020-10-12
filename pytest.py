6d9524d297d6d6d98a476096df3639de9d58cb2d
hernanChain <hernanchain.dev@gmail.com>
2020-10-12 16:16:59 -0500

    Add a brief test

--- pytest.py
+++ pytest.py
@@ -1,1 +1,10 @@
-print("It's working!")
+import unittest
+
+class Test(unittest.TestCase):
+
+    def test_helloWorld(self):
+        message = "Hello World"
+        self.assertEqual("Hello World", message)
+
+if __name__ == "__main__":
+	unittest.main()
